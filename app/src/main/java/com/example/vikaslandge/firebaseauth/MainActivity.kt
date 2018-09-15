92698c4c3c60cff8d9bd214db20b8d3068cd417b
vikasslandge <vikasslandge@gmail.com>
2018-09-15 22:26:01 +0530

    launch/splash screen added

--- app/src/main/java/com/example/vikaslandge/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/vikaslandge/firebaseauth/MainActivity.kt
@@ -11,16 +11,17 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mAuth = FirebaseAuth.getInstance();
         Loginbutton.setOnClickListener {
             mAuth!!.signInWithEmailAndPassword(editTextEmail.text.toString(),editText2Pass.text.toString()).addOnCompleteListener {
                 if (it.isSuccessful){
-                    Toast.makeText(this,"Registration successful",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this,"Login successful",Toast.LENGTH_LONG).show()
                 }
                 else{
-                    Toast.makeText(this,"Registration Fail",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this,"login Fail",Toast.LENGTH_LONG).show()
 
                 }
             }
