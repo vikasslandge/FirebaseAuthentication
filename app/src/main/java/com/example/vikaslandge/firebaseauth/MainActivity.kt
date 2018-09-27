b0afe4822f850ff257e5b579601adf6e6005f58f
vikasslandge <vikasslandge@gmail.com>
2018-09-27 23:51:48 +0530

    launch/splash screen added

--- app/src/main/java/com/example/vikaslandge/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/vikaslandge/firebaseauth/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vikaslandge.firebaseauth
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.auth.FirebaseAuth
@@ -30,12 +31,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
         RegisterButton.setOnClickListener {
-            mAuth!!.createUserWithEmailAndPassword(editTextEmail.text.toString(),editText2Pass.text.toString()).addOnCompleteListener {
-                if (it.isSuccessful){
-                    Toast.makeText(this,"Registration successful",Toast.LENGTH_LONG).show()
-                }
-                else{
-                    Toast.makeText(this,"Registration Fail",Toast.LENGTH_LONG).show()
+            if(editTextEmail.text.toString().isEmpty()&&editText2Pass.text.toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Please fill in the required fields", Toast.LENGTH_SHORT).show();
+
+                //if (editText2Pass.length() < 6) {
+                  //  Toast.makeText(getApplicationContext(), "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth!!.createUserWithEmailAndPassword(editTextEmail.text.toString(), editText2Pass.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this, "Registration successful", Toast.LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(this, "Registration Fail", Toast.LENGTH_LONG).show()
+
+                        }
+
 
                 }
             }
--- app/src/main/java/com/example/vikaslandge/firebaseauth/login.kt
+++ app/src/main/java/com/example/vikaslandge/firebaseauth/login.kt
@@ -7,6 +7,7 @@ class login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_login)
     }
 }
